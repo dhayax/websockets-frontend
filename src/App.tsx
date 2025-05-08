88d0f2907e08beaaf2b98a7679a2a2e52ef5824f
dhayax <dhayanithi756@gmail.com>
2025-05-08 18:55:52 +0530

    initialized a react typescript project and installed tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return <div className="h-screen bg-black">
+<div className="h-[90vh] bg-red-300"></div>
+<div className="">
+  <input type="text" />
+</div>
+  </div>;
+}
+
+export default App;
