c29ada5d92ddcc23d0d67e36578166c9d27df7fa
dhayax <dhayanithi756@gmail.com>
2025-05-10 06:48:01 +0530

    connected the websocket backend with frontend...and implemented all the functionality to send and receive messages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,54 @@
+import { useEffect, useRef, useState } from "react";
+
 function App() {
-  return <div className="h-screen bg-black">
-<div className="h-[90vh] bg-red-300"></div>
-<div className="">
-  <input type="text" />
-</div>
-  </div>;
+  const [messages, setMessages] = useState(["hi there", "hello"]);
+  const wsRef=useRef()
+
+  useEffect(() => {
+    const ws = new WebSocket("http://localhost:8080");
+    ws.onmessage = (event) => {
+      setMessages((m) => [...m, event.data]);
+    };
+    wsRef.current=ws
+    ws.onopen=()=>{
+      ws.send(JSON.stringify({
+        type:"join",
+        payload:{
+          roomId:"red"
+        }
+      }))
+    }
+  }, []);
+
+  return (
+    <div className="h-screen bg-black">
+      <br />
+      <br />
+      <div className="h-[90vh]">
+        {messages.map((message) => (
+      <div className="m-8">
+            <span className=" bg-white text-black rounded p-4 ">
+            {message}
+          </span>
+      </div>
+        ))}
+      </div>
+      <div className="w-full bg-white flex">
+        <input id="message" type="text" className="flex-1 p-4" />
+        <button onClick={()=>{
+          const message=document.getElementById("message").value
+           wsRef.current.send(JSON.stringify({
+type:"chat",
+payload:{
+  message:message
+}
+           }))
+        }} className="bg-purple-600 text-white cursor-pointer p-4">
+          Send Message
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
